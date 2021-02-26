be26689b02565e2686d7134000bec6012a029618
carlbruck <carl.bruckner@gmail.com>
2021-02-26 14:52:52 -0500

    updates

---
+++ KanyeandRonAPI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeandRonAPI
+{
+    class Program
+    {
+        private static string kanyeURL;
+
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var quoteGenerator = new QuoteGenerator(client);
+
+
+            var kanyeQuote = quoteGenerator.KanyeQuote();
+
+            var ronQuote = quoteGenerator.RonQuote();
+
+
+            //var client = new HttpClient();
+
+
+
+            //var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            //var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            //Console.WriteLine($"{kanyeQuote}");
+
+            Console.WriteLine($"_______________________________");
+            Console.WriteLine($"Kanye: '{kanyeQuote}'");
+            Console.WriteLine($"_______________________________");
+
+
+
+
+
+            Console.WriteLine($"_______________________________");
+            Console.WriteLine($"Ron: '{ronQuote}'");
+            Console.WriteLine($"_______________________________");
+        }
+    }
+}
---
+++ KanyeandRonAPI/QuoteGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeandRonAPI
+{
+    public class QuoteGenerator
+    {
+        
+            private string kurl = "https://api.kanye.rest";
+            private string ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            private HttpClient client;
+        public QuoteGenerator(HttpClient newClient)
+        {
+            client = newClient;
+        }
+        public string KanyeQuote()
+        {
+
+
+            var kanyeResponse = client.GetStringAsync(kurl).Result;
+
+            return JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            
+        }
+
+            public string RonQuote()
+        {
+            var client = new HttpClient();
+
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            return JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+           
+
+        }
+
+    }
+}
